0986a446ae4e0532f80373d52b18c4367d328e8e
Terren Culberson <culbersonterren@yahoo.com>
2020-01-10 04:37:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,12 +8,14 @@ class Dog
     @name = name
     @@all << self
   end
+  
   def self.clear_all
     @@all.clear
   end
   
   def self.all
-    @@all.each do |d|
-      puts d.name
+    @@all.each do |dog|
+      puts dog.name
     end
   end
+end
